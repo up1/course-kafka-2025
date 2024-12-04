cdffe6501d65e966cb2aaf3ce6736490cda3db2f
up1 <somkiat.p@gmail.com>
2024-12-04 08:37:02 +0700

    Update producer config

--- workshop/single/demo/cmd/hello_producer.go
+++ workshop/single/demo/cmd/hello_producer.go
@@ -15,6 +15,35 @@ func main() {
 
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": kafkaURL,
+		// Enable the Idempotent Producer
+		// "enable.idempotence": true,
+
+		// acks=1 means the leader will write the record to its local log but will respond without awaiting full acknowledgement from all followers.
+		// acks=1 is the default setting.
+		// acks=0 means the leader will not wait for any acks from the followers.
+		// acks=all means the leader will wait for all in-sync replicas to acknowledge the record.
+		// acks=all is the safest setting that will guarantee no data loss.
+		// acks=all is the slowest setting.
+		"acks": 1,
+
+		// The number of messages allowed on the producer queue.
+		// When this number is reached, the producer will either block or throw an error based on queue.buffering.max.messages.
+		"queue.buffering.max.messages": 1000000,
+
+		// The maximum size of the message queue in bytes.
+		// This property is used to set the maximum amount of memory the producer will use to buffer messages waiting to be sent to the broker.
+		"queue.buffering.max.kbytes": 1000000,
+
+		// The maximum time, in milliseconds, for buffering data on the producer queue.
+		// If this time is exceeded, librdkafka will return an error.
+		"queue.buffering.max.ms": 50,
+
+		// The maximum number of messages batched in one MessageSet.
+		// The total MessageSet size is also limited
+
+		// The maximum size of a message in bytes.
+		// This is the largest size of a message that the broker will allow.
+		"message.max.bytes": 1000000,
 	})
 
 	if err != nil {
