ccef76ddc958796acdc33042381676d812189b72
up1 <somkiat.p@gmail.com>
2024-12-11 09:25:17 +0700

    Demo with consumer

---
+++ workshop/java-spring/consumer/README.md
@@ -0,0 +1,24 @@
+# Run Kafka's producer and consumer
+* [Spring Kafka](https://docs.spring.io/spring-kafka/reference/tips.html)
+
+## Run server
+```
+$gradlew bootRun
+```
+
+## Testing to send data
+```
+$curl --location 'http://localhost:8080/test' \
+--header 'Content-Type: application/json' \
+--data '{
+"message": "demo 01"
+}'
+```
+
+## Build docker image
+```
+$gradlew bootJar
+$docker image build -t consumer:1.0 .
+```
+
+
---
+++ workshop/java-spring/consumer/src/main/kotlin/com/example/producer/HelloConsumer.kt
@@ -0,0 +1,29 @@
+package com.example.producer
+
+import EXAMPLE_TOPIC_NAME
+import GROUP_ID
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.handler.annotation.Header
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Component
+
+@Component
+class ExampleConsumer {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    @KafkaListener(topics = [EXAMPLE_TOPIC_NAME], groupId = GROUP_ID)
+    fun firstListener(
+        @Payload message: String,
+        @Header(KafkaHeaders.RECEIVED_PARTITION) partition: Int,
+        @Header(KafkaHeaders.OFFSET) offsets: Int,
+        // Manual mode
+//        acknowledgment: Acknowledgment
+    ) {
+        logger.info("Message received: [$message] in partition=[$partition] with offset=[$offsets]")
+        // Disable commit
+//        acknowledgment.acknowledge()
+    }
+}
